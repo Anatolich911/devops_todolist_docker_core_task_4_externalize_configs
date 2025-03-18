7116e809bd6e9b717a59f8495977771bbfc345c7
Anatolii <shtyrka.anatoliy@gmail.com>
2025-03-18 12:43:02 -0400

    Docker compose

--- todolist/settings.py
+++ todolist/settings.py
@@ -62,12 +62,12 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'app_db',
-        'USER': 'app_user',
-        'PASSWORD': '1234',
-        'HOST': 'mysql',  # You can use a different host if your MySQL server is on a remote machine.
-        'PORT': '',  # Leave this empty to use the default MySQL port (3306).
+        'ENGINE': os.environ.get ("ENGINE", ""),        ###   We leave this quotes open "" so we can reuse it for a different cases, env
+        'NAME': os.environ.get ("NAME",""),             ###   and in this case we will specify them in docker-compose. Docker file that docker 
+        'USER': os.environ.get("USER", ""),             ###   is using has to be updated to
+        'PASSWORD': os.environ.get ("PASSWORD", ""),
+        'HOST': os.environ.get("HOST", ""),
+        'PORT': os.environ.get ("MYSQL_DB_POR", ""),  # Leave this empty to use the default MySQL port (3306).
     }
 }
 
